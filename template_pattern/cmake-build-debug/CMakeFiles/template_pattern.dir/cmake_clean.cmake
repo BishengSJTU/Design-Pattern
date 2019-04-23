00faa27d6439de880c53d28c5aa82114ed85152d
Chao Li <chao1.li@intel.com>
2019-04-23 14:57:30 +0800

    first commit

---
+++ template_pattern/cmake-build-debug/CMakeFiles/template_pattern.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template_pattern.dir/main.cpp.o"
+  "template_pattern.pdb"
+  "template_pattern"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/template_pattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
